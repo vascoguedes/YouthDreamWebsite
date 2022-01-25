078e91370140ff10866c091e46fee06d8be41cbd
Vasco Guedes <vasco.guedes8@gmail.com>
2022-01-25 10:44:13 +0000

    responsiveness contact

--- src/components/HomeComponents/ContactForm.js
+++ src/components/HomeComponents/ContactForm.js
@@ -28,8 +28,8 @@ function ContactForm() {
                 <h1>ENTRA EM CONTACTO CONNOSCO!</h1>
             </div>
             <div className='row d-flex justify-content-center '>
-                <img src={Email} className='col-2' style={{'object-fit':'contain', 'opacity':'0.8'}} />
-                <form action="https://www.actionforms.io/e/r/vasco-guedes-test" method="any" href="/" className='col-3 p-5'>
+                <img src={Email} className='col-xl-2 col-lg-3 col-md-4 col-sm-5 d-none d-sm-block' style={{'object-fit':'contain', 'opacity':'0.8'}} />
+                <form action="https://www.actionforms.io/e/r/vasco-guedes-test" method="any" href="/" className='col-xl-3 col-lg-4 col-md-5 col-sm-6 p-5'>
                     <input type='text' name='firstName' placeholder='Primeiro Nome' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                     <input type='text' name='lastName' placeholder='Último Nome' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                     <input type='text' name='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
--- src/components/HomeComponents/Projects.js
+++ src/components/HomeComponents/Projects.js
@@ -20,41 +20,42 @@ class Projects extends React.Component {
                             <div className="flip-card-front">
                                 <img src={Nahir} style={{"width":"auto", "height":"100%"}}/>
                             </div>
-                            <div className="flip-card-back">
+                            <div className="flip-card-back row row-centered p-2">
                                 <h1>Nahir e os ODS</h1>
-                                <p>Architect Engineer</p>
-                                <p>We love that guy</p>
-                                <BsInstagram size={25} className='socialMedia' onClick={() => window.open('https://www.instagram.com/diariodanahir/')}/>
-                                <button>Mais info</button>
+                                <p>O Livro pretende levar o conceito de desenvolvimento sustentável ao maior número de crianças possível, sendo transversal aos 17 ODS.</p>
+                                <div>
+                                    <BsInstagram size={25} className='socialMedia' onClick={() => window.open('https://www.instagram.com/diariodanahir/')}/>
+                                    <button onClick={() => window.location.href = "/Nahir&ODS"}>Mais info</button>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="flip-card col-4">
+                    <div className="flip-card col-4 mt-md-0 mt-sm-4">
                         <div className="flip-card-inner">
                             <div className="flip-card-front">
                                 <img src={Education} style={{"width":"auto", "height":"100%"}}/>
                             </div>
-                            <div className="flip-card-back">
+                            <div className="flip-card-back row row-centered p-3">
                                 <h1>Education Builders</h1>
-                                <p>Architect Engineer</p>
-                                <p>We love that guy</p>
-                                <BsInstagram size={25} className='socialMedia' onClick={() => window.open('https://www.instagram.com/educationbuilders/')}/>
-                                <BsFacebook size={25} className='socialMedia' onClick={() => window.open('https://www.facebook.com/EducationBuildersYTHDRM')}/>
-                                <BsLinkedin size={25} className='socialMedia' onClick={() => window.open('https://www.linkedin.com/company/educationbuilders/')}/>
+                                <p>Projeto dedicado ao Sistema Educativo Português que trabalha os ODS 4, 8 e 11.</p>
+                                <div>
+                                    <BsInstagram size={25} className='socialMedia' onClick={() => window.open('https://www.instagram.com/educationbuilders/')}/>
+                                    <BsFacebook size={25} className='socialMedia' onClick={() => window.open('https://www.facebook.com/EducationBuildersYTHDRM')}/>
+                                    <BsLinkedin size={25} className='socialMedia' onClick={() => window.open('https://www.linkedin.com/company/educationbuilders/')}/>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="flip-card col-4">
+                    <div className="flip-card col-4 mt-lg-0 mt-sm-4">
                         <div className="flip-card-inner">
                             <div className="flip-card-front">
                                 <img src={Lixo} style={{"width":"auto", "height":"100%"}}/>
                             </div>
-                            <div className="flip-card-back">
+                            <div className="flip-card-back row row-centered p-3">
                                 <h1>Lixo Não</h1>
-                                <p>Architect Engineer</p>
-                                <p>We love that guy</p>
+                                <p>Projeto que visa o combate à poluição, promovendo os ODS 13, 14 e 15.</p>
                                 <BsInstagram size={25} className='socialMedia' onClick={() => window.open('https://www.instagram.com/lixo_nao/')}/>
                             </div>
                         </div>
