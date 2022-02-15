1bd4f68944c31a9374629dddcc29ea02c0a48652
Vasco Guedes <vasco.guedes8@gmail.com>
2022-02-15 16:53:11 +0000

    changeContact

--- src/components/HomeComponents/ContactForm.js
+++ src/components/HomeComponents/ContactForm.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import './ContactForm.css';
 
 import Email from './../../images/email.png'
+import emailjs from 'emailjs-com'
+
+import{ init } from '@emailjs/browser';
+init("user_43Zv967Kq4VpXGOYMUBgJ");
 
 function ContactForm() {
 
@@ -9,6 +13,34 @@ function ContactForm() {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [questions, setQuestions] = useState('');
+
+    const send = () => {
+
+        const serviceId = 'service_lvbui56';
+        const templateId = 'template_67d6zd2';
+        const userId = 'user_43Zv967Kq4VpXGOYMUBgJ';
+        const templateParams = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            questions: questions,
+        };
+    
+        emailjs
+            .send(serviceId, templateId, templateParams, userId)
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+                console.log('FAILED...', error);
+            })
+    
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setQuestions('');
+
+        window.location.href = '/';
+    }
 
     return (
         <div id='contact' className='form-container-reg d-flex justify-content-center row row-centered m-0'>
@@ -29,13 +61,13 @@ function ContactForm() {
             </div>
             <div className='d-flex justify-content-center '>
                 <img src={Email} className='col-xl-2 col-lg-3 col-md-4 col-sm-5 d-none d-sm-block' style={{'object-fit':'contain', 'opacity':'0.8'}} />
-                <form action="https://www.actionforms.io/e/r/vasco-guedes-test" method="any" href="/" className='col-xl-3 col-lg-4 col-md-5 col-sm-6 p-5'>
+                <div method="any" href="/" className='col-xl-3 col-lg-4 col-md-5 col-sm-6 p-5'>
                     <input type='text' name='firstName' placeholder='Primeiro Nome' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                     <input type='text' name='lastName' placeholder='Último Nome' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                     <input type='text' name='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <input type='text' name='questions' placeholder='Perguntas, dúvidas...' value={questions} onChange={(e) => setQuestions(e.target.value)}/>
-                    <button className='submit'>Enviar</button>
-                </form>
+                    <button className='submit' onClick={send}>Enviar</button>
+                </div>
             </div>
         </div>
     )
